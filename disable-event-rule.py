18bfe80c7fade8866d919714b91f07852d251b0d
tepnimitl <13294585+tepnimitl@users.noreply.github.com>
2020-12-22 11:44:21 -0500

    Create disable-event-rule.py

---
+++ disable-event-rule.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+
+client = boto3.client('events')
+sns = boto3.client('sns')
+rule_name = 'StartStop-Scheduler-SchedulerRule-xxx'
+sns_arn = 'arn:aws:sns:us-east-1:xxx:event-scheduler-status'
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    response = client.disable_rule(
+        Name = rule_name
+    )
+    status = client.describe_rule(
+        Name = rule_name
+        )
+        
+    status = status['State']
+    print(rule_name +str(" : ") +str(status))
+    
+    response = sns.publish(
+        TargetArn = sns_arn,
+        Subject = 'xxx Scheduler Status',
+        Message = 'xxx Scheduler Status : ' + status
+    )
+    
+    return {
+        'statusCode': 200,
+        'rule_name': rule_name,
+        'status': status,
+        'body': json.dumps('Successfully!')
+    }
